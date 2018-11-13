c903e115b3af25b72bbcc93616287d64533a3eb3
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-13 17:27:44 -0500

    Adds JS

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+var dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight(){
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(leftNumbers, 10)
+  
+  if(right > 0){
+    dodger.style.right ='$(left + 1)px'
+  }
 }
