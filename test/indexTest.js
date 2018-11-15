a3de21f54c5d3ab3bd411c09021b84edba84ddb1
Kellye Greene <kellye.greene@flatironschool.com>
2018-11-15 13:53:32 -0500

    Adds starting tests code

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,8 +1,37 @@
-describe('index', () => {
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+describe('Move', () => {
+  afterEach(function() {
+    expect.restoreSpies()
   })
-  it('runs', () => {
-    expect(true).toBe.true
+
+  describe('moveDodgerLeft()', () => {
+    beforeEach(() => {
+      dodger = document.getElementById('dodger')
+    })
+
+    it('moves the DODGER to the left', () => {
+      const left = positionToInteger(dodger.style.left)
+
+      moveDodgerLeft()
+
+      expect(positionToInteger(dodger.style.left)).toBeLessThan(left)
+    })
+  })
+
+  describe('moveDodgerRight', () => {
+    beforeEach(() => {
+      dodger = document.getElementById('dodger')
+
+      window.requestAnimationFrame = cb => {
+        cb()
+      }
+    })
+
+    it('moves the DODGER to the right', () => {
+      const left = positionToInteger(dodger.style.left)
+
+      moveDodgerRight()
+
+      expect(positionToInteger(dodger.style.left)).toBeGreaterThan(left)
+    })
   })
 })
