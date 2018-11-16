3919d8d4c4e54c3abf9977301e5373427794f1e2
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-16 16:36:23 -0500

    Gets actual tests working

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,8 +1,35 @@
-describe('index', () => {
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+describe('moveDodgerLeft()', () => {
+  beforeEach(() => {
+    dodger = document.getElementById('dodger')
   })
-  it('runs', () => {
-    expect(true).toBe.true
+
+  it('moves the DODGER to the left', () => {
+    var left = dodger.style.left
+    left = parseInt(left)
+
+    moveDodgerLeft()
+
+    var newPosition = dodger.style.left
+    newPosition = parseInt(newPosition)
+
+    expect(newPosition).to.be.below(left)
+  })
+})
+
+describe('moveDodgerRight', () => {
+  beforeEach(() => {
+    dodger = document.getElementById('dodger')
+  })
+
+  it('moves the DODGER to the right', () => {
+    var left = dodger.style.left
+    left = parseInt(left)
+
+    moveDodgerRight()
+
+    var newPosition = dodger.style.left
+    newPosition = parseInt(newPosition)
+
+    expect(newPosition).to.be.above(left)
   })
 })
