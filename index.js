997d95101f3340cf53d89dd84c84dc5f59797f61
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-16 16:36:42 -0500

    Adds working solution

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-
-var dodger = document.getElementById('dodger')
+var dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
+  var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10)
-
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
 
-function moveDodgerRight(){
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(leftNumbers, 10)
-  
-  if(right > 0){
-    dodger.style.right ='$(left + 1)px'
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
   }
 }
+
+document.addEventListener('keydown', function(key) {
+  if (key.which === 37) {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(key) {
+  if (key.which === 39) {
+    moveDodgerRight()
+  }
+})
