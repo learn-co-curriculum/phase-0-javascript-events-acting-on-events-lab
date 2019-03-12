ca62fda106df418b16b592ba8cc342f1c2335daf
Steven G. Harms <steven.harms@flatironschool.com>
2019-03-12 12:10:50 -0400

    Use event.key versus event.which

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+var dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
