524073188cf46370db8b12991ca04fdf7a885f40
Liz Burton <liz.burton147@gmail.com>
2021-06-04 14:19:43 -0400

    AUTO: remove file-system from test files

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+var dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
