2093d1b32b6f60cca52596b59e9d18749a9e9aee
drake bishop <drakemx85@gmail.com>
2023-01-25 02:11:16 -0600

    commit tree 4785d341018b13c608115db3a43e38d74bcacbac

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 20);
+  
+    if (right > 0) {
+      dodger.style.left = `${right - 1}px`;
+    }
+  }
