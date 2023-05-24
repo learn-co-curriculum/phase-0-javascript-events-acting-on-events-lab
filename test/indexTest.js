cbe908f09f9ac2d9bfaf7e6f1123e69b406d24aa
Matt Eva <matthew.eva@flatironschool.com>
2023-05-24 15:23:40 -0700

    adding spaces for legibility

--- test/indexTest.js
+++ test/indexTest.js
@@ -13,6 +13,7 @@ describe('moveDodgerLeft()', () => {
 
     let newPosition = dodger.style.left
     newPosition = parseInt(newPosition)
+
     if (left > 0){
       expect(newPosition).to.be.below(left)
     } else {
@@ -34,6 +35,7 @@ describe('moveDodgerRight()', () => {
 
     let newPosition = dodger.style.left
     newPosition = parseInt(newPosition)
+    
     if (left < 360){
       expect(newPosition).to.be.above(left)
     } else{
