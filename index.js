6b44a14ee3b89d0ae33a57e94a1e5e773a1e7653
Sean <ohsean812@gmail.com>
2023-06-21 23:48:51 -0400

    commit tree 5849ee6ec62cf6c0ed4bfc811ecf3405c80267c2

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const dodger = document.getElementById("dodger")
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "")
+  const left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left -1}px`
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+})
+
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "")
+  const right = parseInt(rightNumbers, 10)
+
+  if (right < 360) {
+    dodger.style.left = `${right + 1}px`
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+})
