2fe2323a26a657c307635e851bd294fd70d4ed65
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-10-22 17:26:26 -0400

    the one

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ function moveDodgerLeft() {
       moveDodgerLeft();
     }
   });
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) { // Adjusted for a 40px-wide dodger in a 400px-wide game field
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
