63bd825b91dd1916d5901c58ddc6a091b1dfdba5
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-10-22 17:04:40 -0400

    js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
